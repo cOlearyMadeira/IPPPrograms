3ebf9ba17aa23d1aa13b41be3c447e48a9527d19
cOlearyMadeira <76957741+cOlearyMadeira@users.noreply.github.com>
2021-01-05 10:24:03 -0500

    Add files via upload

---
+++ jan52021shortquestions.py
@@ -0,0 +1,25 @@
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+
+    # statement 1
+    user_name = input("What's your name?")
+    print(user_name)
+
+    # statement 2
+    user_age = input("How old are you?")
+    print(user_age)
+
+    # statement 3
+    user_hometown = input("Where are you from")
+    print(user_hometown)
+
+    # statement 4
+    user_siblings = input("how many siblings do you have?")
+    print(user_siblings)
+
+    # statement 5
+    user_sport = input("what's your favorite sport to play?")
+    print(user_sport)
+
+
